4bda7a7cb1b9fcf1fa663f6b059a3e157d5e0429
eric <eric.thomas@formation-industries-fc.fr>
2020-10-02 13:36:48 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+
+    char devinette='8';  //attention char pour le bon fonctionnement de isdigit
+    char saisie=' ';
+    debut:  //marqueur de debut pour le GOTO
+    printf("Veuillez saisir un nombre entre 1 et 9\n");
+    scanf(" %c",&saisie);
+    if(!isdigit(saisie)) //si le saisie contient un caractere
+    {
+        printf("Le nombre n'est pas numérique\n");
+
+        goto debut;
+    }
+    else
+    {
+        if(devinette==saisie) //si le saisie est égale à la devinette
+        {
+            printf("Vous avez gagné");
+        }
+        else
+        {
+            printf("Vous n'avez pas trouvé le nombre\n");
+            goto debut;
+        }
+    }
+    return 0;
+}
